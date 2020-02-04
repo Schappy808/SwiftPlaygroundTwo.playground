e876bd1e760474e8aa2ba881908009eeae3960b9
Schappy808 <59860894+Schappy808@users.noreply.github.com>
2020-02-04 10:10:12 -1000

    Completed parts 1-4

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var goodGrade = "I’ve been doing all my work"
+if goodGrade == "I'm not doing very well"
+{
+    print("I should be getting an A at this point")
+}
+
+else
+
+{
+    print("I need to work harder in this class")
+}
+
+var gradeA = true
+
+if gradeA
+{
+    print("I really lile this class")
+}
+
+else
+
+{ print("I'm not so sure about coding")}
